63d4642b00248fc2c8de376ec267d8c937de311f
hnin3i <hninei1897@gmail.com>
2026-09-03 15:21:57 +0630

    Add post Controller

---
+++ src/main/java/ai/myapp/controller/PostController.java
@@ -0,0 +1,14 @@
+package ai.myapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class PostController {
+	
+	@GetMapping("/posts")
+	public String postList() {
+		
+		
+		return "posts/list";
+	}
+
+}
